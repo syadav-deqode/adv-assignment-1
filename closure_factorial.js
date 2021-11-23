90c7e8f5e9dea71f0a238d3e86b242abbeed3494
syadav-deqode <syadav@deqode.com>
2021-11-23 13:25:37 +0530

    Minimize the code with arrow function and ternary operator.

--- closure_factorial.js
+++ closure_factorial.js
@@ -1,10 +1,18 @@
-const factorialOfNumber = function (n) {
-  if (n > 1) {
-    return n * factorialOfNumber(n - 1)
-  } else {
-    return 1
-  }
-}
+// const factorialOfNumber = function (n) {
+//   if (n > 1) {
+//     return n * factorialOfNumber(n - 1)
+//   } else {
+//     return 1
+//   }
+// }
+
+/**
+ * We can do above code in minimal way.
+ * Using arrow function.
+ * Using ternary operator
+*/
+
+const factorialOfNumber = (n) => n > 1 ? n * factorialOfNumber(n - 1) : 1;
 
 const output = factorialOfNumber(3)
 
